52360a1dad4efe4df88c122ca841f90a49f322ca
Bill Wang <bill.wang>
2019-07-23 23:54:41 +1000

    hello lambda

--- lib/cdk-hello-stack.ts
+++ lib/cdk-hello-stack.ts
@@ -1,18 +1,19 @@
-import sns = require('@aws-cdk/aws-sns');
-import subs = require('@aws-cdk/aws-sns-subscriptions');
-import sqs = require('@aws-cdk/aws-sqs');
 import cdk = require('@aws-cdk/core');
+import lambda = require('@aws-cdk/aws-lambda');
 
 export class CdkHelloStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+    
+    // defines an AWS Lambda resource
+    const hello = new lambda.Function(this, 'HelloHandler', {
 
-    const queue = new sqs.Queue(this, 'CdkHelloQueue', {
-      visibilityTimeout: cdk.Duration.seconds(300)
-    });
+      runtime: lambda.Runtime.NODEJS_10_X,
+      code: lambda.Code.asset('lambda'),
+      handler: 'hello.handler',
+      description: 'lambda demo with cdk'
+    }
 
-    const topic = new sns.Topic(this, 'CdkHelloTopic');
-
-    topic.addSubscription(new subs.SqsSubscription(queue));
+    )
   }
 }
