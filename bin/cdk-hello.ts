0d22629c0e3dbf6b8db3626901a8780240202771
Bill Wang <bill.wang>
2019-07-24 11:11:23 +1000

    Add tslint

--- bin/cdk-hello.ts
+++ bin/cdk-hello.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
-import cdk = require('@aws-cdk/core');
-import { CdkHelloStack } from '../lib/cdk-hello-stack';
+import cdk = require("@aws-cdk/core");
+import { CdkHelloStack } from "../lib/cdk-hello-stack";
 
 const app = new cdk.App();
-new CdkHelloStack(app, 'CdkHelloStack');
+new CdkHelloStack(app, "CdkHelloStack");
--- lib/cdk-hello-stack.ts
+++ lib/cdk-hello-stack.ts
@@ -1,19 +1,18 @@
-import cdk = require('@aws-cdk/core');
-import lambda = require('@aws-cdk/aws-lambda');
+import lambda = require("@aws-cdk/aws-lambda");
+import cdk = require("@aws-cdk/core");
 
 export class CdkHelloStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-    
+
     // defines an AWS Lambda resource
-    const hello = new lambda.Function(this, 'HelloHandler', {
-
+    const hello = new lambda.Function(this, "HelloHandler", {
+      code: lambda.Code.asset("lambda"),
+      description: "lambda demo with cdk",
+      handler: "hello.handler",
       runtime: lambda.Runtime.NODEJS_10_X,
-      code: lambda.Code.asset('lambda'),
-      handler: 'hello.handler',
-      description: 'lambda demo with cdk'
-    }
+    },
 
-    )
+    );
   }
 }
