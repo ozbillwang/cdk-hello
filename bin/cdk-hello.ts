1ee2916b087a6c0aaead6dbc58fdebbff935a8db
Bill Wang <bill.wang>
2019-08-03 14:24:42 +1000

    Clean hello codes

--- bin/cdk-hello.ts
+++ bin/cdk-hello.ts
@@ -1,8 +1,6 @@
 #!/usr/bin/env node
 import cdk = require("@aws-cdk/core");
-import { CdkHelloStack } from "../lib/cdk-hello-stack";
 import { CdkWorkshopStack } from "../lib/cdk-workshop-stack";
 
 const app = new cdk.App();
-// new CdkHelloStack(app, "CdkHelloStack");
 new CdkWorkshopStack(app, "CdkWorkshopStack");
